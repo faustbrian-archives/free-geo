c96ace9b9433cbea9441fc9de67671c6160b6e2f
Brian Faust <hello@brianfaust.de>
2017-07-20 11:48:54 +0300

    Refactor to use faustbrian/http

---
+++ src/API/Location.php
@@ -0,0 +1,29 @@
+<?php
+
+/*
+ * This file is part of FreeGeo PHP Client.
+ *
+ * (c) Brian Faust <hello@brianfaust.me>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace BrianFaust\FreeGeo\API;
+
+use BrianFaust\Http\HttpResponse;
+
+class Location extends AbstractAPI
+{
+    /**
+     * Get details about the given ip address.
+     *
+     * @param string $addr
+     *
+     * @return \BrianFaust\Http\HttpResponse
+     */
+    public function details(string $addr): HttpResponse
+    {
+        return $this->client->get($addr);
+    }
+}
